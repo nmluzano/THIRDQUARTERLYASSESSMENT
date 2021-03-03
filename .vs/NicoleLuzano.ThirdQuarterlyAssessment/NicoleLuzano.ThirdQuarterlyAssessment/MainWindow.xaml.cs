8e14373b140cc0fbcc85ae5e63f79805cc017cd5
Nicole Luzano <nmluzano@csmbataan.ph.education>
2021-03-03 18:29:02 +0800

    added longitude and latitude.

--- .vs/NicoleLuzano.ThirdQuarterlyAssessment/NicoleLuzano.ThirdQuarterlyAssessment/MainWindow.xaml.cs
+++ .vs/NicoleLuzano.ThirdQuarterlyAssessment/NicoleLuzano.ThirdQuarterlyAssessment/MainWindow.xaml.cs
@@ -23,6 +23,46 @@ namespace NicoleLuzano.ThirdQuarterlyAssessment
         public MainWindow()
         {
             InitializeComponent();
+            List<string> municipalities = new List<string>()
+            {
+                "Dinalupihan",
+                "Davao City",
+                "Angeles City",
+                "Brooke's Point",
+                "Olongapo City"
+            };
+            cboMunicipalities.ItemsSource = municipalities;
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cboMunicipalities.SelectedIndex == 0)
+            {
+                myMap.Center = new Microsoft.Maps.MapControl.WPF.Location(14.8781,120.4546);
+                myMap.ZoomLevel = 13;
+            }
+            if (cboMunicipalities.SelectedIndex == 1)
+            {
+                myMap.Center = new Microsoft.Maps.MapControl.WPF.Location(7.130099278801634, 125.42804470063739);
+                myMap.ZoomLevel = 13;
+            }
+            if (cboMunicipalities.SelectedIndex == 2)
+            {
+                myMap.Center = new Microsoft.Maps.MapControl.WPF.Location(15.160460231120194, 120.60047399746576);
+                myMap.ZoomLevel = 13;
+            }
+            if (cboMunicipalities.SelectedIndex == 3)
+            {
+                myMap.Center = new Microsoft.Maps.MapControl.WPF.Location(8.827339952813013, 117.83041789939554);
+                myMap.ZoomLevel = 13;
+            }
+            if (cboMunicipalities.SelectedIndex == 4)
+            {
+                myMap.Center = new Microsoft.Maps.MapControl.WPF.Location(14.834821290849918, 120.2820978394843);
+                myMap.ZoomLevel = 13;
+            }
+
         }
     }
 }
